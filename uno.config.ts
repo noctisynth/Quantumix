1ad6440d29b18968910a5cc7bb83c456fc640d07
苏向夜 <fu050409@163.com>
2024-05-15 18:12:16 +0800

    chore: update vite config

---
+++ uno.config.ts
@@ -0,0 +1,22 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+} from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetUno({
+      attributifyPseudo: true,
+    }),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+    }),
+    presetTypography(),
+    presetWebFonts(),
+  ],
+});
